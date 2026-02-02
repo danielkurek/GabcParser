68a94d82526529f81b3d6c8846231519988a0701
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-02-02 21:55:30 +0100

    Fix: compatibility with python 3.10

--- gabcparser/GabcParser.py
+++ gabcparser/GabcParser.py
@@ -8,7 +8,7 @@ def load_parser(variation: Literal['gabc', 'mei-gabc', 's-gabc'], **kwargs) -> L
         raise TypeError("`variation` should be of type str")
     if variation not in grammars.supported_grammars:
         raise ValueError(f"Invalid value of `variation`. Supported types {', '.join(f'`{x}`' for x in grammars.supported_grammars)}")
-    grammar_path = importlib.resources.files() / "grammars" / f"{variation}.lark"
+    grammar_path = importlib.resources.files(__name__.split(".", maxsplit=1)[0]) / "grammars" / f"{variation}.lark"
     with grammar_path.open("r", encoding="utf-8") as f:
         parser = Lark(f, **kwargs)
         return parser
