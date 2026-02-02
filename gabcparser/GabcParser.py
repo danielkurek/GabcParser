75b9d343e618e3c69f4a33a95e786fa955d61d1b
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-02-02 21:41:20 +0100

    Fix: remove backslash inside f-string for broader compatibility

--- gabcparser/GabcParser.py
+++ gabcparser/GabcParser.py
@@ -7,8 +7,10 @@ def load_parser(variation: Literal['gabc', 'mei-gabc', 's-gabc'], **kwargs) -> L
     if not isinstance(variation, str):
         raise TypeError("`variation` should be of type str")
     if variation not in grammars.supported_grammars:
-        raise ValueError(f"Invalid value of `variation`. Supported types {', '.join(f'\'{x}\'' for x in grammars.supported_grammars)}")
+        raise ValueError(f"Invalid value of `variation`. Supported types {', '.join(f'`{x}`' for x in grammars.supported_grammars)}")
     grammar_path = importlib.resources.files() / "grammars" / f"{variation}.lark"
     with grammar_path.open("r", encoding="utf-8") as f:
         parser = Lark(f, **kwargs)
         return parser
+
+load_parser('gabc')
