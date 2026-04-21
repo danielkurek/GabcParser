82b0daea0864d75cf6f55890c290e99da52a9952
Daniel Kurek <daniel.kurek.dev@gmail.com>
2026-04-21 15:31:00 +0200

    add common grammar to supported grammars

--- gabcparser/grammars/__init__.py
+++ gabcparser/grammars/__init__.py
@@ -1,5 +1,6 @@
 GABC = "gabc"
 MEI_GABC = "mei-gabc"
 S_GABC = "s-gabc"
+COMMON = "common-gabc"
 
-supported_grammars = [GABC, MEI_GABC, S_GABC]
+supported_grammars = [GABC, MEI_GABC, S_GABC, COMMON]
